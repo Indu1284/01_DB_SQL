c5d0c2437520a808dcab49b02ed3064aa188cdad
Indu1284 <indurevathy@gmail.com>
2021-09-13 07:47:40 +0000

    updated

--- 05_SQL_SELECT/05_2_que_basic.sql
+++ 05_SQL_SELECT/05_2_que_basic.sql
@@ -1,8 +1,17 @@
 
 
-SELECT * FROM stocks.ccc LIMIT 5;
+#SELECT * FROM stocks.ccc LIMIT 5;
 
-
+SELECT
+ticker AS "SYM",
+price AS "Kurs in $",
+c_name AS "Unternehmen",
+industry AS "Branche"
+FROM stocks.ccc
+LIMIT 10
+#LIMIT 5; 5 from 0
+#LIMIT 200,10; # 10 lines starting from 200 th row.
+;
 
 
 
